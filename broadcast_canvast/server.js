63dc1c6c272ead614e1a8d3ee02beeaaad27e760
HalynLe <24521260@gm.uit.edu.vn>
2026-05-03 13:19:50 +0700

    Update server.js

--- broadcast_canvast/server.js
+++ broadcast_canvast/server.js
@@ -5,19 +5,14 @@ const io = require("socket.io")(3000, {
 console.log("Server running at http://localhost:3000");
 
 io.on("connection", (socket) => {
-  console.log("User connected");
 
-  // join room
   socket.on("join_room", (room_id) => {
     socket.join(room_id);
   });
 
-  // nhận draw → broadcast lại
   socket.on("draw", (msg) => {
+    // broadcast cho người khác trong room
     socket.to(msg.room_id).emit("draw", msg);
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
 });
